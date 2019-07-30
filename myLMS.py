b34b68166e1811aa1d9c5aa80d87552524488b19
Patrick Medeiros De Luca <pjepk2@gmail.com>
2019-07-30 14:15:10 -0300

    Nothing new

---
+++ myLMS.py
@@ -0,0 +1,19 @@
+import numpy as np
+import math 
+import matplotlib.pyplot as plt
+import random as rand
+
+N = 2000 #iteraction number
+
+R = 10 #Realization number
+
+mu = 1.0*10**(-) #step-size
+
+#w0 =  np.array([[ 1],[-2], [-0.1], [0.2], [math.pi], [math.exp(-2.6)], [-math.sqrt(3)]])
+
+sigma_z = 1*10**(-2) #standart deviation of the noise
+sigma_x = 1.0 #standart deviation of the input signal
+
+
+
+#adaptative alorithm
