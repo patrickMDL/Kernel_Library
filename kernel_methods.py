35ba5a2f13a2c8800b08c986af9abd4d904bbfdd
Patrick Medeiros De Luca <pjepk2@gmail.com>
2019-08-13 17:23:08 -0300

    KLMS fixed (I think)

--- kernel_methods.py
+++ kernel_methods.py
@@ -1,10 +1,9 @@
 import numpy as np
-e = 2.718281828
-
+e = 2.718281828459045235360287
 def Linear_K( x, y, c):
     #x and y are the vector, while c is the constant
     #k(x,y)=x^T.y + c
-   result = y.dot(x.T) + c
+   result = y.T * x  + c
    return result
 
 def Polynomial_K(x, y, a, c, d):
@@ -13,46 +12,39 @@ def Polynomial_K(x, y, a, c, d):
     return result
 
 def Gaussian_K (x, y, s):
-   #S stands for sigma and e is the euler number
-    x=x-y
-    result = e**(-1*(np.linalg.norm(x)**2/2*(s**2)))
-    return result
+   #S stands for sigma 
+   norm = np.linalg.norm(x-y)
+   div = 2*(s**2)
+   exponencial = (norm**2 / div) * -1
+   result = np.exp(exponencial)
+   return result
 
 def Exponential_K(x, y, s):
-   x = x-y
-   result = e**(-1*(np.linalg.norm(x)/2*(s**2)))
+   result = np.exp(-1*(np.linalg.norm(x-y)/2*(s**2)))
    return result
 
 def Laplacian_K(x, y, s):
-   x = x-y
-   result = e**(-1*(np.linalg.norm(x)/s))
+   result = np.exp**(-1*(np.linalg.norm(x-y)/s))
    return result
 
 def Sigmoid_K(x, y, a, c):
-   result = (a*y.dot(x.T))+c
-   result = np.sinh(result)/np.cosh(result)
+   result = np.tanh(a*x.T*y+c)
    return result
 
 def Rational_Quadratic_K(x, y, c):
-   x=x-y
-   result = 1 - ((np.linalg.norm(x)**2)/((np.linalg.norm(x)**2)+c))
+   result = 1 - ((np.linalg.norm(x-y)**2)/((np.linalg.norm(x-y)**2)+c))
    return result
 
 def Multiquadric_K (x, y, c):
-   norm = x-y
-   norm = np.linalg.norm(norm)**2 + c**2
-   result = np.sqrt(norm)
+   result = np.sqrt(np.linalg.norm(x-y)**2 + c**2)
    return result
 
 def Inverse_Multiquadric_K(x,y,c):
-   norm = x-y
-   norm = np.linalg.norm(norm)**2 + c**2
-   result = 1/np.sqrt(norm)
+   result = 1/np.sqrt(np.linalg.norm(x-y)**2 + c**2)
    return result
 
 def Cauchy_K(x, y, s):
-   x=x-y
-   result = (np.linalg.norm(x)**2/(s**2))
+   result = (np.linalg.norm(x-y)**2/(s**2))
    result = 1 + result
    result = 1 / result
    return result
